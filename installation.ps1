c840b7df3a5d380520baee07c50e9d83cb870d49
Shansai Muraleetharan <61542805+TheGreatShan@users.noreply.github.com>
2024-02-26 20:41:37 +0100

    Create installation.ps1

---
+++ installation.ps1
@@ -0,0 +1,18 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+scoop install git
+
+scoop bucket add main
+scoop install main/dotnet-sdk
+
+dotnet --version
+
+scoop bucket add java
+scoop install java/openjdk17
+
+scoop bucket add extras
+scoop install extras/rider
+
+dotnet workload install maui
+dotnet workload install wasm-tools
